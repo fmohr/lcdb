e115ed2e244d2f79d5fcbfde0ec61b8264adfa19
Tom Viering <TommieV@Gmail.com>
2023-07-28 16:14:05 +0200

    deactivating hps only possible on newer version..

--- publications/2023-neurips/lcdb/workflow/_util.py
+++ publications/2023-neurips/lcdb/workflow/_util.py
@@ -219,7 +219,7 @@ def get_default_config(config_space):
         default = hyperparameter.default_value
         defaulthps[hyperparameter_name] = default
     default_config = ConfigSpace.configuration_space.Configuration(config_space, values=defaulthps, allow_inactive_with_values=True)
-    default_config = config_space.deactivate_inactive_hyperparameters(default_config, config_space)
+    # default_config = config_space.deactivate_inactive_hyperparameters(default_config, config_space) # only available in later version...
     return default_config
 
 
