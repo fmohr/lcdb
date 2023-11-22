63895cea180d3c9eb676711d258a7b0165ef649d
Deathn0t <romainegele@gmail.com>
2023-11-22 16:08:16 +0100

    update

--- publications/2023-neurips/lcdb/cli/_run.py
+++ publications/2023-neurips/lcdb/cli/_run.py
@@ -334,7 +334,7 @@ def main(
         mpi4py.rc.initialize = False
         mpi4py.rc.threads = True
         mpi4py.rc.thread_level = "multiple"
-        # mpi4py.rc.recv_mprobe = False
+        mpi4py.rc.recv_mprobe = False
 
         from mpi4py import MPI
 
