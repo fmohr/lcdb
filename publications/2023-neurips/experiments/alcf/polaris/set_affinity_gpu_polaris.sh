77cc8e8ea28ef42729454716aa805f4fca85408d
Deathn0t <romainegele@gmail.com>
2023-11-23 19:33:36 +0000

    update scripts

--- publications/2023-neurips/experiments/alcf/polaris/set_affinity_gpu_polaris.sh
+++ publications/2023-neurips/experiments/alcf/polaris/set_affinity_gpu_polaris.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-num_gpus=4
-gpu=$((${PMI_LOCAL_RANK} % ${num_gpus}))
+
+gpu=$((${PMI_LOCAL_RANK} % ${NGPUS_PER_NODE}))
 export CUDA_VISIBLE_DEVICES=$gpu
 echo “RANK= ${PMI_RANK} LOCAL_RANK= ${PMI_LOCAL_RANK} gpu= ${gpu}”
 exec "$@"
