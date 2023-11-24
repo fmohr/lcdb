60fd73c80944622b9c2f2d94ccc638d1990b8ee2
Deathn0t <romainegele@gmail.com>
2023-11-24 15:46:12 +0000

    update

--- publications/2023-neurips/experiments/alcf/polaris/randomforest/create.sh
+++ publications/2023-neurips/experiments/alcf/polaris/randomforest/create.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -e
+set -xe
 
 # Load Python Environment
 source /lus/grand/projects/datascience/regele/polaris/lcdb/publications/2023-neurips/build/activate-dhenv.sh
 
 # Load Experiment Configuration
-source ./config.sh
+source config.sh
 
 # Create Configurations
 lcdb create -w $LCDB_WORKFLOW -n $LCDB_NUM_CONFIGS -o $LCDB_INITIAL_CONFIGS
