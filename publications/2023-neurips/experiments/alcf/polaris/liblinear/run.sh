a4dded251a5372d02cc7b5b0412d9508e372f870
Deathn0t <romainegele@gmail.com>
2023-11-30 09:07:24 +0000

    adding liblinear resutls in polaris

---
+++ publications/2023-neurips/experiments/alcf/polaris/liblinear/run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#PBS -l select=2:system=polaris
+#PBS -l place=scatter
+#PBS -l walltime=00:60:00
+#PBS -q debug
+#PBS -A datascience
+#PBS -l filesystems=grand:home
+
+set -e
+
+cd ${PBS_O_WORKDIR}
+
+source /lus/grand/projects/datascience/regele/polaris/lcdb/publications/2023-neurips/build/activate-dhenv.sh
+
+#!!! CONFIGURATION - START
+source ./config.sh
+
+export timeout=3500
+
+export NDEPTH=2
+export NRANKS_PER_NODE=16
+export NNODES=`wc -l < $PBS_NODEFILE`
+export NTOTRANKS=$(( $NNODES * $NRANKS_PER_NODE))
+export OMP_NUM_THREADS=$NDEPTH
+#!!! CONFIGURATION - END
+
+mkdir -p $LCDB_OUTPUT_RUN
+pushd $LCDB_OUTPUT_RUN
+
+# Run experiment
+mpiexec -n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} --depth=${NDEPTH} --cpu-bind depth --envall \
+    lcdb run \
+    --openml-id $LCDB_OPENML_ID \
+    --workflow-class $LCDB_WORKFLOW \
+    --monotonic \
+    --max-evals $LCDB_NUM_CONFIGS \
+    --timeout $timeout \
+    --initial-configs $LCDB_INITIAL_CONFIGS \
+    --timeout-on-fit 300 \
+    --workflow-seed $LCDB_WORKFLOW_SEED \
+    --valid-seed $LCDB_VALID_SEED \
+    --test-seed $LCDB_TEST_SEED \
+    --evaluator mpicomm
+
+gzip --best results.csv
