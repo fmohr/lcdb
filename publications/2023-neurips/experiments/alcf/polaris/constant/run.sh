75f1632de1601f98df86735378f6507cfa3e6d5c
Deathn0t <romainegele@gmail.com>
2023-12-08 15:46:56 +0000

    running constant predictor on all datasets with different seeds

--- publications/2023-neurips/experiments/alcf/polaris/constant/run.sh
+++ publications/2023-neurips/experiments/alcf/polaris/constant/run.sh
@@ -2,7 +2,7 @@
 #PBS -l select=2:system=polaris
 #PBS -l place=scatter
 #PBS -l walltime=00:60:00
-#PBS -q debug-scaling
+#PBS -q debug
 #PBS -A datascience
 #PBS -l filesystems=grand:home
 
@@ -26,13 +26,16 @@ export LCDB_WORKFLOW=lcdb.workflow.sklearn.ConstantWorkflow
 export LCDB_OUTPUT_WORKFLOW=$PWD/output/$LCDB_WORKFLOW
 export LCDB_OPENML_IDS=(3 6 11 12 14 15 16 18 22 23 28 29 31 32 37 38 44 50 54 151 182 188 300 307 458 469 554 1049 1050 1053 1063 1067 1068 1461 1462 1464 1468 1475 1478 1480 1485 1486 1487 1489 1494 1497 1501 1510 1590 4134 4534 4538 23381 23517 40499 40668 40670 40701 40923 40927 40975 40978 40979 40982 40983 40984 40996 41027)
 export LCDB_WORKFLOW_SEED=0
-export LCDB_VALID_SEEDS=(0 1 2 3 4)
-export LCDB_TEST_SEEDS=(0 1 2 3 4)
 
 # Loop
-for LCDB_OPENML_ID in $LCDB_OPENML_IDS do
+for LCDB_OPENML_ID in ${LCDB_OPENML_IDS[@]};
+do
+    
+    export LCDB_OPENML_ID
 
     echo "Running experiment for dataset $LCDB_OPENML_ID"
+
+    lcdb fetch --task-id openml.$LCDB_OPENML_ID
 
     mpiexec -n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} --depth=${NDEPTH} \
         --cpu-bind depth --envall ./test.sh
--- publications/2023-neurips/experiments/alcf/polaris/constant/test.sh
+++ publications/2023-neurips/experiments/alcf/polaris/constant/test.sh
@@ -3,27 +3,27 @@
 if [ $PMI_RANK -lt 25 ]
 then
 
-    LCDB_VALID_SEEDS_IDX=$(( ${PMI_RANK} % 5 ))
-    LCDB_TEST_SEEDS_IDX=$(( ${PMI_RANK} / 5 % 5 ))
-    export LCDB_VALID_SEED=${LCDB_VALID_SEEDS[LCDB_VALID_SEEDS_IDX]}
-    export LCDB_TEST_SEED=${LCDB_TEST_SEEDS[LCDB_TEST_SEEDS_IDX]}
+    export LCDB_VALID_SEED=$(( ${PMI_RANK} % 5 ))
+    export LCDB_TEST_SEED=$(( ${PMI_RANK} / 5 % 5 ))
 
-    LCDB_OUTPUT_DATASET=$LCDB_OUTPUT_WORKFLOW/$LCDB_OPENML_ID
-    LCDB_OUTPUT_RUN=$LCDB_OUTPUT_DATASET/$LCDB_VALID_SEED-$LCDB_TEST_SEED-$LCDB_WORKFLOW_SEED
+    export LCDB_OUTPUT_DATASET=$LCDB_OUTPUT_WORKFLOW/$LCDB_OPENML_ID
+    export LCDB_OUTPUT_RUN=$LCDB_OUTPUT_DATASET/$LCDB_VALID_SEED-$LCDB_TEST_SEED-$LCDB_WORKFLOW_SEED
 
     echo "Output in $LCDB_OUTPUT_RUN"
 
-    # mkdir -p $LCDB_OUTPUT_RUN
-    # pushd $LCDB_OUTPUT_RUN
+    mkdir -p $LCDB_OUTPUT_RUN
+    pushd $LCDB_OUTPUT_RUN
 
-    # # Run experiment
-    # lcdb test --openml-id $LCDB_OPENML_ID \
-    #     --workflow-class $LCDB_WORKFLOW \
-    #     --monotonic \
-    #     --valid-seed $LCDB_VALID_SEED \
-    #     --test-seed $LCDB_TEST_SEED \
-    #     --workflow-seed $LCDB_WORKFLOW_SEED > output.json
+    # Run experiment
+    lcdb test --openml-id $LCDB_OPENML_ID \
+        --workflow-class $LCDB_WORKFLOW \
+        --monotonic \
+        --valid-seed $LCDB_VALID_SEED \
+        --test-seed $LCDB_TEST_SEED \
+        --workflow-seed $LCDB_WORKFLOW_SEED > output.json
 
-    # gzip --best output.json
+    gzip --best output.json
+
+    popd
 
 fi
