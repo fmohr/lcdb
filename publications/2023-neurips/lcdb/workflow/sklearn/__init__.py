bfe8cb418c8029dc99904ff73f595ed8e74f1495
Deathn0t <romainegele@gmail.com>
2023-12-08 12:33:17 +0100

    adding constant predictor

--- publications/2023-neurips/lcdb/workflow/sklearn/__init__.py
+++ publications/2023-neurips/lcdb/workflow/sklearn/__init__.py
@@ -1,8 +1,15 @@
 """Sub-package for sklearn models.
 """
+from ._constant import ConstantWorkflow
 from ._knn import KNNWorkflow
 from ._libsvm import LibSVMWorkflow
 from ._liblinear import LibLinearWorkflow
 from ._randomforest import RandomForestWorkflow
 
-__all__ = ["KNNWorkflow", "LibSVMWorkflow", "LibLinearWorkflow", "RandomForestWorkflow"]
+__all__ = [
+    "ConstantWorkflow",
+    "KNNWorkflow",
+    "LibSVMWorkflow",
+    "LibLinearWorkflow",
+    "RandomForestWorkflow",
+]
