a345ec27ac95cfe0ad56d930d444e7e66b40d891
Deathn0t <romainegele@gmail.com>
2023-12-18 11:59:57 -0500

    updated fetch command to show split

--- publications/2023-neurips/lcdb/cli/_fetch.py
+++ publications/2023-neurips/lcdb/cli/_fetch.py
@@ -1,11 +1,7 @@
 """Command line to create a list of hyperparameter configurations to be evaluated later."""
-import logging
-import os
-import pathlib
-
 import numpy as np
-import pandas as pd
 from lcdb.data import load_task
+from lcdb.data.split import train_valid_test_split
 
 
 def add_subparser(subparsers):
@@ -25,12 +21,30 @@ def add_subparser(subparsers):
         required=True,
         help="The task ID. For example, the task 61 from OpenML will have task ID 'openml.61'",
     )
+    subparser.add_argument(
+        "-vp",
+        "--valid-prop",
+        type=float,
+        default=0.1,
+        required=False,
+        help="Ratio of validation/(train+validation+test).",
+    )
+    subparser.add_argument(
+        "-tp",
+        "--test-prop",
+        type=float,
+        default=0.1,
+        required=False,
+        help="Ratio of test/(train+validation+test).",
+    )
 
     subparser.set_defaults(func=function_to_call)
 
 
 def main(
     task_id: str,
+    valid_prop: float,
+    test_prop: float,
 ):
     """Entry point for the command line interface."""
 
@@ -38,11 +52,38 @@ def main(
     (X, y), dataset_metadata = load_task(task_id)
     print(" done!\n")
 
+    (
+        X_train,
+        X_valid,
+        X_test,
+        y_train,
+        y_valid,
+        y_test,
+    ) = train_valid_test_split(
+        X,
+        y,
+        test_seed=42,
+        valid_seed=42,
+        test_prop=test_prop,
+        valid_prop=valid_prop,
+        stratify=True,
+    )
+
     print(f" * Type   : {dataset_metadata['type']}")
     print(f" * X shape: {np.shape(X)}")
     print(f" * y shape: {np.shape(y)}")
     print(f" * Classes: {dataset_metadata['num_classes']}")
     print(f" * Categories: {dataset_metadata['categories']}")
+
+    print()
+    print(" --- Splitting ---")
+
+    print(f" * Train X shape: {np.shape(X_train)}")
+    print(f" * Train y shape: {np.shape(y_train)}")
+    print(f" * Valid X shape: {np.shape(X_valid)}")
+    print(f" * Valid y shape: {np.shape(y_valid)}")
+    print(f" * Test  X shape: {np.shape(X_test)}")
+    print(f" * Test  y shape: {np.shape(y_test)}")
 
     print()
     print(" --- Description ---")
