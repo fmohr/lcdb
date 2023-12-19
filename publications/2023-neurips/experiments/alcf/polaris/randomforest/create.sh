af37599da172b31f2c5c056571a589e1795ac971
Deathn0t <romainegele@gmail.com>
2023-12-19 19:24:48 +0000

    updated create script

--- publications/2023-neurips/experiments/alcf/polaris/randomforest/create.sh
+++ publications/2023-neurips/experiments/alcf/polaris/randomforest/create.sh
@@ -15,6 +15,7 @@ lcdb create -w $LCDB_WORKFLOW -n $LCDB_NUM_CONFIGS -o $LCDB_INITIAL_CONFIGS
 # Fetch Datasets
 for LCDB_OPENML_ID in ${LCDB_OPENML_ID_ARRAY[@]};
 do
-    echo "Fetching dataset $LCDB_OPENML_ID
+    echo "Fetching dataset $LCDB_OPENML_ID..."
     lcdb fetch --task-id openml.$LCDB_OPENML_ID
+    echo ""
 done
