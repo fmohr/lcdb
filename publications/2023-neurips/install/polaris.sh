503c0da44b1636f44a297ffb0c7a0ef7922ca0a3
Deathn0t <romainegele@gmail.com>
2024-03-12 18:20:11 +0100

    cleaning script for polaris installation

--- publications/2023-neurips/install/polaris.sh
+++ publications/2023-neurips/install/polaris.sh
@@ -13,37 +13,16 @@ set -xe
 
 # Load modules available on the current system
 module load PrgEnv-gnu/8.3.3
-module load llvm/release-15.0.0
 module load conda/2023-10-04
 
 # Copy the base conda environment
-#conda create -p dhenv --clone base -y
+conda create -p dhenv --clone base -y
 conda activate dhenv/
-#pip install --upgrade pip
-
-# Install RedisJSON with Spack
-# Install Spack
-#git clone -c feature.manyFiles=true https://github.com/spack/spack.git
-#. ./spack/share/spack/setup-env.sh
-
-#git clone https://github.com/deephyper/deephyper-spack-packages.git
-
-# Create and activate the `redisjson` environment
-#spack env create redisjson
-#spack env activate redisjson
-
-# Add the DeepHyper Spack packages to the environment
-#spack repo add deephyper-spack-packages
-
-# Add the `redisjson` Spack package to the environment
-#spack add redisjson
-
-# Build the environment
-#spack install
+pip install --upgrade pip
 
 # Install the DeepHyper's Python package
 git clone -b develop git@github.com:deephyper/deephyper.git
-pip install -e "deephyper/[hps,mpi,redis-hiredis]"
+pip install -e "deephyper/[hps,mpi]"
 
 # For mpi4py
 module swap PrgEnv-nvhpc PrgEnv-gnu
@@ -62,23 +41,5 @@ echo "#!/bin/bash" >> activate-dhenv.sh
 # Append modules loading and conda activation
 echo "" >> activate-dhenv.sh
 echo "module load PrgEnv-gnu/8.3.3" >> activate-dhenv.sh
-echo "module load llvm/release-15.0.0" >> activate-dhenv.sh
 echo "module load conda/2023-10-04" >> activate-dhenv.sh
 echo "conda activate $PWD/dhenv/" >> activate-dhenv.sh
-
-# Append Spack activation
-echo "" >> activate-dhenv.sh
-echo ". $PWD/spack/share/spack/setup-env.sh" >> activate-dhenv.sh
-echo "spack env activate redisjson" >> activate-dhenv.sh
-
-# Create Redis configuration
-touch redis.conf
-
-# Accept all connections from the network
-echo "bind 0.0.0.0" >> redis.conf
-
-# Add the RedisJSON module to the configuration file
-cat $(spack find --path redisjson | grep -o "/.*/redisjson.*")/redis.conf >> redis.conf
-
-# Disable protected mode (i.e., no password required when connecting to Redis)
-echo "protected-mode no" >> redis.conf
