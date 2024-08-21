f34f0ffb77b5a31a87e29f4656669fbec073c3df
janvanrijn <janvanrijn@gmail.com>
2024-08-21 15:12:01 +0200

    incorporated config space object

--- publications/2023-neurips/experiments/surf/snellius/analysis/hyperparameter_importance.py
+++ publications/2023-neurips/experiments/surf/snellius/analysis/hyperparameter_importance.py
@@ -1,4 +1,5 @@
 import argparse
+import ConfigSpace.hyperparameters
 import fanova
 import lcdb.db
 import lcdb.builder.utils
@@ -20,15 +21,34 @@ def parse_args():
     parser.add_argument('--openml_taskid_name', type=str, default="m:openmlid")
     parser.add_argument('--output_directory', type=str, default=os.path.expanduser('~/experiments/lcdb'))
     parser.add_argument('--output_filetype', type=str, choices=['pdf', 'png'], default='png')
+    parser.add_argument('--max_load', type=int, default=None)
     return parser.parse_args()
 
 
-def fanova_on_task(task_results, performance_column_name, n_trees):
+def numeric_encode(df, config_space):
+    # https://automl.github.io/ConfigSpace/latest/api/ConfigSpace/configuration_space/
+    result = np.zeros((len(df), len(config_space.get_hyperparameters())), dtype=float)
+
+    for hyperparameter_name, hyperparameter in config_space.items():
+        index = config_space.get_idx_by_hyperparameter_name(hyperparameter_name)
+        if isinstance(hyperparameter, ConfigSpace.hyperparameters.NumericalHyperparameter):
+            result[:, index] = df[hyperparameter_name].to_numpy()
+        elif isinstance(hyperparameter, ConfigSpace.hyperparameters.Constant):
+            result[:, index] = [0] * len(df)
+        elif isinstance(hyperparameter, ConfigSpace.hyperparameters.CategoricalHyperparameter):
+            lst = [hyperparameter.choices.index(value) for value in df[hyperparameter_name].to_numpy()]
+            result[:, index] = lst
+        else:
+            raise ValueError('unsupported hyperparameter type: %s' % str(hyperparameter))
+    return result
+
+
+def fanova_on_task(task_results, performance_column_name, config_space, n_trees):
     fanova_results = []
 
     # in this simplified example, we only display numerical and float hyperparameters. For categorical
     # hyperparameters, the fanova library needs to be informed by using a configspace object.
-    task_results = task_results.select_dtypes(include=["int64", "float64"])
+    # task_results = task_results.select_dtypes(include=["int64", "float64"])
 
     # query_confusion_matrix_values = lcdb.analysis.json.QueryMetricValuesFromAnchors("confusion_matrix", split_name="val")
     # out = task_results[performance_column_name].apply(query_confusion_matrix_values)
@@ -37,20 +57,13 @@ def fanova_on_task(task_results, performance_column_name, n_trees):
     #    out.apply(lambda x: list(map(lambda x: 1 - lcdb.analysis.score.balanced_accuracy_from_confusion_matrix(x), x))).to_list())
     # print(balanced_error_rate_values_for_config.mean(axis=0))
     # print(out)
-
-    # drop rows with unique values. These are by definition not an interesting hyperparameter, e.g., ``axis``,
-    # ``verbose``.
-    columns = [
-        c
-        for c in list(task_results)
-        if len(task_results[c].unique()) > 1 and c != performance_column_name
-    ]
     evaluator = fanova.fanova.fANOVA(
-        X=task_results[columns].to_numpy(),
+        X=numeric_encode(task_results, config_space),
         Y=task_results[performance_column_name].to_numpy(),
+        config_space=config_space,
         n_trees=n_trees,
     )
-    for idx, pname in enumerate(columns):
+    for idx, pname in enumerate(config_space.get_hyperparameter_names()):
         logging.info('-- hyperparameter %d %s' % (idx, pname))
         unique_values = task_results[pname].unique()
         logging.info('-- UNIQUE VALUES: %d (%s)' % (len(unique_values), unique_values))
@@ -72,10 +85,12 @@ def run(args):
 
     WorkflowClass = lcdb.builder.utils.import_attr_from_module(args.workflow_name)
     config_space = WorkflowClass.config_space()
+    workflow_hyperparameter_mapping = {"p:" + name: name for name in config_space.get_hyperparameter_names()}
     id_results = dict()
     print(config_space)  # TODO: properly integrate to fanova
 
-    all_results_all_workflows = lcdb.db.LCDB().get_results(workflows=[args.workflow_name], openmlids=args.openml_ids)
+    all_results_all_workflows = lcdb.db.LCDB().query(workflows=[args.workflow_name], openmlids=args.openml_ids)
+    load_count = 0
     for frame_workflow_job_task in all_results_all_workflows:
         workflow_ids = frame_workflow_job_task['m:workflow'].unique()
         openml_task_ids = frame_workflow_job_task['m:openmlid'].unique()
@@ -86,17 +101,20 @@ def run(args):
             id_results[(workflow_ids[0], openml_task_ids[0])] = list()
         id_results[(workflow_ids[0], openml_task_ids[0])].append(frame_workflow_job_task)
 
+        load_count += 1
+        if args.max_load and load_count >= args.max_load:
+            break
+
     task_ids = set()
     for idx, (workflow_name, task_id) in enumerate(id_results):
         task_ids.add(task_id)
-        all_results = pd.concat(id_results[(workflow_name, task_id)])
-        hyperparameter_names = [i for i in all_results.columns.values if i.startswith('p:')]
-        relevant_columns = hyperparameter_names + [performance_column, args.openml_taskid_name]
-        all_results = all_results[relevant_columns]
+        task_results = pd.concat(id_results[(workflow_name, task_id)])
+        task_results = task_results.rename(workflow_hyperparameter_mapping, axis=1)
+        relevant_columns = list(workflow_hyperparameter_mapping.values()) + [performance_column, args.openml_taskid_name]
+        task_results = task_results[relevant_columns]
 
         logging.info("Starting with task %d (%d/%d)" % (task_id, idx + 1, len(id_results)))
-        task_results = all_results[all_results[args.openml_taskid_name] == task_id]
-        fanova_task_results = fanova_on_task(task_results, performance_column, args.n_trees)
+        fanova_task_results = fanova_on_task(task_results, performance_column, config_space, args.n_trees)
         fanova_all_results.extend(fanova_task_results)
 
     fanova_all_results = pd.DataFrame(fanova_all_results)
