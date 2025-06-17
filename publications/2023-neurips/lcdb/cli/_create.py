fbf504b33c9cb77732610cade52eaa1c70410b0c
Felix Mohr <mail@felixmohr.de>
2025-06-17 15:33:26 +0200

    created logic to enforce a certain number of default configs in lcdb create for preprocessors or learners

--- publications/2023-neurips/lcdb/cli/_create.py
+++ publications/2023-neurips/lcdb/cli/_create.py
@@ -19,6 +19,9 @@ def add_subparser(subparsers):
 
     subparser.add_argument("-w", "--workflow-class", type=str, required=True)
     subparser.add_argument("-n", "--num-configs", type=int, required=True)
+    subparser.add_argument("-ndp", "--num-configs-with-default-preprocessor", type=int, required=False, default=0)
+    subparser.add_argument("-ndl", "--num-configs-with-default-learner", type=int, required=False, default=0)
+    subparser.add_argument("-ed", "--exclude-default-config", action="store_true", required=False, default=False)
     subparser.add_argument(
         "-c", "--campaign", type=str, required=False, default=None
     )
@@ -36,6 +39,9 @@ def add_subparser(subparsers):
 def main(
     workflow_class,
     num_configs,
+    num_configs_with_default_preprocessor,
+    num_configs_with_default_learner,
+    exclude_default_config,
     output_file,
     seed=0,
     verbose=False,
@@ -56,26 +62,49 @@ def main(
     if verbose:
         print(config_space)
 
+    num_fully_random_configs = num_configs - num_configs_with_default_preprocessor - num_configs_with_default_learner
+    if num_fully_random_configs < 0:
+        raise ValueError(f"num_configs must be at least as high as the sum of num_configs_with_default_preprocessor and num_configs_with_default_learner")
+
     # Convert the config space to a skopt space
     skopt_space = convert_to_skopt_space(config_space, surrogate_model="RF")
 
     # Sample the configurations
     # TODO: LHS should be done here
-    configs = skopt_space.rvs(n_samples=num_configs - 1, random_state=seed)
+    configs = skopt_space.rvs(n_samples=num_configs if exclude_default_config else num_configs - 1, random_state=seed)
 
-    # Add the default configuration
-    config_default = config_space.get_default_configuration()
-    x = []
+    # create the default configuration
+    default_config_raw = config_space.get_default_configuration()
+    default_config = []
     for i, k in enumerate(skopt_space.dimension_names):
         # Check if hyperparameter k is active
         # If it is not active we attribute the "lower bound value" of the space
         # To avoid duplication of the same "entity" in the list of configurations
-        if k in dict(config_default):
-            val = config_default[k]
+        if k in dict(default_config_raw):
+            val = default_config_raw[k]
         else:
             val = skopt_space.dimensions[i].bounds[0]
-        x.append(val)
-    configs.insert(0, x)  # at the beginning
+        default_config.append(val)
+    
+    # overwrite some of the configs with default learer values
+    if num_configs_with_default_learner > 0:
+
+        default_learner_params = {i: v for i, (k, v) in enumerate(zip(skopt_space.dimension_names, default_config)) if not k.startswith("pp@")}
+        for c in configs[:num_configs_with_default_learner]:
+            for i, v in default_learner_params.items():
+                c[i] = v
+
+    # overwrite some of the configs with default learer values
+    if num_configs_with_default_preprocessor > 0:
+
+        default_pp_params = {i: v for i, (k, v) in enumerate(zip(skopt_space.dimension_names, default_config)) if k.startswith("pp@")}
+        for c in configs[num_configs_with_default_learner:num_configs_with_default_learner + num_configs_with_default_preprocessor]:
+            for i, v in default_pp_params.items():
+                c[i] = v
+
+
+    if not exclude_default_config:
+        configs.insert(0, default_config)  # at the beginning
 
     # modify output file
     if campaign is not None:
