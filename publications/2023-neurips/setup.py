485b0683587faf1248ead976fb875600625e0bd0
Felix Mohr <mail@felixmohr.de>
2025-09-24 22:19:18 +0200

    fixed required deephyper version

--- publications/2023-neurips/setup.py
+++ publications/2023-neurips/setup.py
@@ -21,7 +21,7 @@ platform_infos = platform.platform()
 # What packages are required for this module to be executed?
 REQUIRED = [
     "ConfigSpace~=1.1.1",
-    "deephyper",
+    "deephyper==0.10.2",
     "jmespath==1.0.1",
     "numpy<2.0",
     "openml",
