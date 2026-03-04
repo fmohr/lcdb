4890146ec788d905fb8265c2ca8566343c33a7c5
Cheng Yan <c.yan-1@tudelft.nl>
2026-03-04 15:14:07 +0100

    update runtime and SBUs estimation

--- publications/2023-neurips/experiments/surf/snellius/download_data.py
+++ publications/2023-neurips/experiments/surf/snellius/download_data.py
@@ -25,11 +25,11 @@ lcdb = LCDB()
 
 for workflow_class in [
     "lcdb.workflow.sklearn.KNNWorkflow",
-    #"lcdb.workflow.sklearn.LibLinearWorkflow",
-    #"lcdb.workflow.sklearn.LibSVMWorkflow",
-    #"lcdb.workflow.sklearn.TreesEnsembleWorkflow",
-    #"lcdb.workflow.xgboost.XGBoostWorkflow",
-    #"lcdb.workflow.keras.DenseNNWorkflow"
+    "lcdb.workflow.sklearn.LibLinearWorkflow",
+    "lcdb.workflow.sklearn.LibSVMWorkflow",
+    "lcdb.workflow.sklearn.TreesEnsembleWorkflow",
+    "lcdb.workflow.xgboost.XGBoostWorkflow",
+    # "lcdb.workflow.keras.DenseNNWorkflow"
 ]:
 
     file = Path(f"{OUTPUT_DIR}/{workflow_class}.jsonl")
@@ -43,14 +43,14 @@ for workflow_class in [
 
         gen = lcdb.query(
             campaigns=[campaign_name],
-            openmlids=[3, 12, 23, 31, 54],
+            openmlids=[3, 12, 23, 31, 54, 181, 188, 1049, 1067, 1111, 1169, 1457, 1461, 1464, 1468, 1475, 1486, 1487, 1489, 1494, 1515, 1590, 1596, 4134, 4135, 4534, 4538, 4541, 23517, 40498, 40668, 40670, 40685, 40701, 40900, 40975, 40978, 40981, 40982, 40983, 40984, 40996, 41027, 41138, 41142, 41143, 41144, 41145, 41146, 41147, 41150, 41156, 41157, 41158, 41159, 41161, 41162, 41163, 41164, 41165, 41166, 41167, 41168, 41169, 42732, 42733, 42734, 42742, 42746, 42769],
             workflows=[workflow_class],
             processors=[
                 LearningCurveExtractor(metrics=["error_rate"], encode_as_json_str=True),
                 compute_payload,
                 TracebackExtractor(),
                 DataMemoryComputer(),
-                #RuntimeExtractor(),
+                # RuntimeExtractor(),
                 extract_anticipated_memory
             ]
         )
